3750599eccb43104b65c0d8e0cc03eaddae71d0f
Edonis Karaqica <edoniskaraqica@gmail.com>
2017-10-02 14:03:08 +0200

    email got updated

--- raportet/raportet1.php
+++ raportet/raportet1.php
@@ -146,7 +146,7 @@ $(document).ready(function(){
                      }elseif (isset($_GET['kidselect'])) {
                       $sql2 = "UPDATE tblkerkesat set approved=1,shqyrtuar=1 where kid='" . $_GET['kidselect'] . "'";
                       $query2 = mysqli_query($conn,$sql2);
-                      //include("../aprovimi_me_email.php");
+                      include("../aprovimi_me_email.php");
                        include("staff_nga_kerkesat.php");
 
                      }
